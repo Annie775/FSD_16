799d2a9a115c63ce704ed7cd85f2486df9a3818c
Ani75 <aniamitgupta@gmail.com>
2024-07-16 16:46:07 +0530

    second commit

---
+++ loops.py
@@ -0,0 +1,33 @@
+
+a = 'Anirudh'
+
+for i in a:
+    print(i)
+
+j=11
+
+for j in range(10):
+    print('Hello ',j)
+    
+items = ['phone', 'shoes', 'watch', 'pen']
+
+for k in items:
+    print(k)
+    for a in k:
+        print(a)
+
+for g in range(5, 50, 5):
+    print(g)
+l=0
+while l<10:
+    print(l)
+    l+=1
+
+for i in range(1,17):
+    if(i%2 == 0):
+        continue
+    print("7 * ",i," = ", 7*i)
+
+# Break = to skip the Loop
+# continue = to skip the iteration    
+    
